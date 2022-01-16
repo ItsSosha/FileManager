6b6f9af3c14bca32dc7ad34bd037aaeb9832c029
ItsSosha <whoissosha@gmail.com>
2022-01-16 18:36:46 +0200

    Added unit tests, moved commands to the separate file CommandsLib.cs
    Внес все правки из предыдущего ПР, а так же немного подправил код в соответствии с пилюлей

---
+++ FileManager/CommandsLib.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CommandsLib
+{
+    public class Commands
+    {
+        public static void GetFileProperties(FileInfo file)
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine($"File name: {file.Name}");
+            Console.WriteLine($"File size: {file.Length} bytes");
+            Console.WriteLine($"File extension: {file.Extension}");
+            Console.WriteLine($"File creation time: {file.CreationTime}");
+        }
+
+        public void CheckAndChangeDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Environment.CurrentDirectory = (path);
+            }
+            else
+            {
+                Console.WriteLine($"Invalid directory path {path}");
+            }
+        }
+
+        public string ReadFile(string F)
+        {
+            StreamReader streamr;
+            try
+            {
+                streamr = new StreamReader($@"{Environment.CurrentDirectory}\{F}");
+            }
+            catch
+            {
+                Console.WriteLine($"Invalid file name {F}");
+                return "";
+            }
+            char[] read = new char[200];
+            streamr.Read(read, 0, 200);
+            read.PrintAllElements<char>('\0');
+            string output = new string(read).Trim('\0');
+            return output;
+        }
+
+        public string GetFiles(string mode = "-a")
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            switch (mode)
+            {
+                case "-a":
+                    string[] fileNames = new string[1];
+                    foreach (string file in files)
+                    {
+                        string fileName = file.Split('\\')[file.Split('\\').Length - 1];
+                        Console.WriteLine(fileName);
+                        if (fileNames[fileNames.Length - 1] != null)
+                        {
+                            fileNames = fileNames.Push<string>(fileName);
+                        }
+                        else
+                        {
+                            fileNames[Array.IndexOf(fileNames, null)] = fileName;
+                        }
+                    }
+                    fileNames = fileNames.Where(e => !String.IsNullOrEmpty(e)).ToArray();
+                    string result = String.Join(", ", fileNames);
+                    return String.Join(", ", result);
+                case "-t":
+                    foreach (string file in files)
+                    {
+                        FileInfo f = new FileInfo(file);
+                        GetFileProperties(f);
+                    }
+                    break;
+                case "-s":
+                    FileInfo[] fs = new FileInfo[files.Length];
+                    for (int i = 0; i < fs.Length; i++)
+                    {
+                        FileInfo f = new FileInfo(files[i]);
+                        fs[i] = f;
+                    }
+
+                    for (int j = 1; j < fs.Length; j++)
+                    {
+                        for (int i = 0; i < fs.Length - j; i++)
+                        {
+                            if (fs[i].Length > fs[i + 1].Length)
+                            {
+                                FileInfo temp = fs[i + 1];
+                                fs[i + 1] = fs[i];
+                                fs[i] = temp;
+                            }
+                        }
+                    }
+
+                    string[] res = new string[fs.Length];
+                    int k = 0;
+                    foreach (FileInfo f in fs)
+                    {
+                        Console.WriteLine($"{f.Name}: {f.Length} bytes");
+                        res[k] = $"{f.Name}: {f.Length} bytes";
+                        k++;
+                    }
+                    return String.Join(", ", res);
+            }
+            return "";
+        }
+
+        public void CreateDir(string newDir)
+        {
+            string path = $@"{Environment.CurrentDirectory}\{newDir}";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            else
+            {
+                Console.WriteLine($"The subdirectory {newDir} already exists in the current directory");
+            }
+        }
+
+        public bool CreateFile(string newFile)
+        {
+            string path = $@"{Environment.CurrentDirectory}\{newFile}";
+            if (!File.Exists(path))
+            {
+                try
+                {
+                    File.Create(path);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"File {newFile} already exists in the current directory");
+                return true;
+            }
+
+        }
+
+        public bool DeleteDir(string newDir, string flag = "-f")
+        {
+            bool check = flag == "-t" ? true : false;
+            string path = $@"{Environment.CurrentDirectory}\{newDir}";
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.Delete(path, check);
+                    return true;
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine($"The subdirectory {path} is not empty. Use -t flag to confirm its deletion");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No directory {newDir} to delete in {Environment.CurrentDirectory}");
+                return true;
+            }
+        }
+
+        public bool DeleteFile(string F)
+        {
+            string path = $@"{Environment.CurrentDirectory}\{F}";
+            if (File.Exists(path))
+            {
+                try
+                {
+                    System.GC.Collect();
+                    System.GC.WaitForPendingFinalizers();
+                    File.Delete(path);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message, "\\", e.InnerException, "\\", e.Source);
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No file {F} to delete in {Environment.CurrentDirectory}");
+                return true;
+            }
+        }
+
+        public bool RenameFile(string oldFile, string newFile)
+        {
+            string path1 = $@"{Environment.CurrentDirectory}\{oldFile}";
+            if (File.Exists(path1))
+            {
+                string path2 = $@"{Environment.CurrentDirectory}\{newFile}";
+                File.Move(path1, path2, true);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"No file {newFile} to rename in {Environment.CurrentDirectory}");
+                return false;
+            }
+        }
+
+        public void RenameDir(string oldPath, string newDir)
+        {
+            string newPath = $@"{Directory.GetParent(Environment.CurrentDirectory)}\{newDir}";
+            try
+            {
+                Directory.Move(oldPath, newPath);
+            }
+            catch
+            {
+                Console.WriteLine($"The given directory {oldPath} can't be changed.");
+            }
+        }
+
+        public bool SearchFile(string f, string subStr)
+        {
+            StreamReader streamr;
+            try
+            {
+                streamr = new StreamReader($@"{Environment.CurrentDirectory}\{f}");
+            }
+            catch
+            {
+                Console.WriteLine($"Invalid file name {f}");
+                return false;
+            }
+
+            string[] text = streamr.ReadToEnd().Split("\r\n");
+            int[] lines = new int[0];
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Contains(subStr))
+                {
+                    lines = lines.Push<int>(i + 1);
+                }
+            }
+
+            lines = lines.Where(e => e != 0).ToArray();
+            if (lines.Length != 0)
+            {
+                Console.WriteLine($"The substring was found in lines: {String.Join(", ", lines)}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"The given file contains no substring {subStr}");
+                return false;
+            }
+        }
+
+
+    }
+
+    public static class ArrayExtensions
+    {
+        public static T[] Push<T>(this T[] arr, T element) where T : IComparable<T>
+        {
+            int last = arr.Length;
+            Array.Resize(ref arr, arr.Length + 20);
+            arr[last] = element;
+            return arr;
+        }
+
+        public static void PrintAllElements<T>(this T[] arr, T stop) where T : IComparable<T>
+        {
+            for (int i = 0; i < arr.Length && arr[i].CompareTo(stop) > 0; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ FileManager/Program.cs
@@ -0,0 +1,105 @@
+﻿
+/*
+ * Для некоторых команд требуется перейти непосредственно в директорию, где находится нужный файл/директория, над которыми будет производиться операция. Но поддержку полного пути добавлю тоже.
+ * cd [Путь] - изменить директорию
+ * getFiles - вывод файлов директории (флаги: -a (по умолчанию) - в алфавитном порядке; -t - в виде древа с информацией; -s - сортировка по размеру)
+ * read [Имя файла] - прочитать первые 200 символов файла из открытой директории
+ * createDir [Имя директории] [Флаг -t или -f] - создать директорию в открытой директории. Флаг показ
+ * createFile [Имя файла] - создать файл в открытой директории
+ * delDir [Имя директории] - удалить дочернюю директорию открытой директории
+ * delFile [Имя файла] - удалить файл из открытой директории
+ * renameFile [Старое имя файла] [Новое имя файла] - переименовать файл в открытой директории
+ * renameDir [Путь со старым именем] [Путь с новым именем] - переименовать директорию, принимает только полные пути, т. е. открытие родительской директории необязательно
+ * search - поиск по файлу (выводит строки в которых есть нужная подстрока)
+ * exit - завершить консольное приложение
+ * Спасибо!
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using CommandsLib;
+
+namespace FileManager
+{
+    class Program
+    {
+        public static void ExecuteCommand(string[] Input)
+        {
+            Commands ConsoleCommand = new Commands();
+            switch (Input[0])
+            {
+                case "cd":
+                    string Path = Input[1];
+                    ConsoleCommand.CheckAndChangeDirectory(Path);
+                    break;
+                case "getFiles":
+                    if (Input.Length > 1)
+                    {
+                        ConsoleCommand.GetFiles(Input[1]);
+                    }
+                    else
+                    {
+                        ConsoleCommand.GetFiles();
+                    }
+                    break;
+                case "read":
+                    ConsoleCommand.ReadFile(Input[1]);
+                    break;
+                case "createDir":
+                    ConsoleCommand.CreateDir(Input[1]);
+                    break;
+                case "createFile":
+                    ConsoleCommand.CreateFile(Input[1]);
+                    break;
+                case "delDir":
+                    if (Input.Length > 2)
+                    {
+                        ConsoleCommand.DeleteDir(Input[1], Input[2]);
+                    }
+                    else
+                    {
+                        ConsoleCommand.DeleteDir(Input[1]);
+                    }
+                    break;
+                case "delFile":
+                    ConsoleCommand.DeleteFile(Input[1]);
+                    break;
+                case "renameFile":
+                    ConsoleCommand.RenameFile(Input[1], Input[2]);
+                    break;
+                case "renameDir":
+                    ConsoleCommand.RenameDir(Input[1], Input[2]);
+                    break;
+                case "search":
+                    ConsoleCommand.SearchFile(Input[1], Input[2]);
+                    break;
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine($"Invalid command {Input[0]}");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string CurrentDirectory = Directory.GetCurrentDirectory();
+                Console.Write(CurrentDirectory + "> ");
+                string[] Input = Console.ReadLine().Split(' ').Where(e => e != "").ToArray();
+                try
+                {
+                    ExecuteCommand(Input);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Missing arguments");
+                }
+            }
+        }
+
+    }
+}
+
